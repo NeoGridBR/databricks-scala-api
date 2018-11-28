f6304b2a75e3119d1d14937eb28f7d27e7a62b10
Leonardo Alves Neuwald <leonardo.neuwald@neogrid.com>
2018-11-28 16:34:55 -0200

    Add support to Runs methods

---
+++ src/main/scala/io/findify/databricks/api/Submit.scala
@@ -0,0 +1,14 @@
+package io.findify.databricks.api
+
+case class Submit (run_name: Option[String],
+                   existing_cluster_id: Option[String] = None,
+                   new_cluster: Option[Cluster] = None,
+                   notebook_task: Option[NotebookTask] = None,
+                   spark_jar_task: Option[SparkJarTask] = None,
+                   spark_python_task: Option[SparkPythonTask] = None,
+                   spark_submit_task: Option[SparkSubmitTask] = None,
+                   timeout_seconds: Option[Int] = None,
+                   libraries: Option[List[Library]] = None
+                  )
+
+case class SubmitResponse(job_id:Int)
---
+++ src/test/scala/io/findify/databricks/DataTest.scala
@@ -0,0 +1,5 @@
+package io.findify.databricks
+
+class DataTest {
+
+}
